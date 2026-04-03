4842e4b966d7ff3ec12bc60ac28d5b03e547f11f
attiquehorizon <93094957+Mehar-habib@users.noreply.github.com>
2026-04-03 09:40:34 +0500

    Two-Factor Authentication Part 1 & 2

---
+++ app/Mail/VerificationCodeMail.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class VerificationCodeMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public string $code;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(int|string $code)
+    {
+        $this->code = (string) $code;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Your Login Verification Code Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'email.verification_code',
+            with: ['code' => $this->code],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -3,7 +3,7 @@
     <head>
 
         <meta charset="utf-8" />
-        <title>Log In </title>
+        <title>Verification page </title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc."/>
         <meta name="author" content="HabibThemes"/>
@@ -36,7 +36,7 @@
                                     </div>
 
                                     <div class="pt-0">
-                                        <form method="POST"  action="{{route('admin.login')}}" class="my-4">
+                                        <form method="POST"  action="{{route('custom.verification.verify')}}" class="my-4">
                                             @csrf
                                             @if (session("error"))
                                                 <div class="alert alert-danger">
@@ -44,37 +44,17 @@
                                                 </div>
                                             @endif
                                             <div class="form-group mb-3">
-                                                <label for="emailaddress" class="form-label">Email address</label>
-                                                <input class="form-control" type="email" name="email" id="emailaddress" required="" placeholder="Enter your email">
-                                                @error('email')
+                                                <label for="code" class="form-label">verification code</label>
+                                                <input class="form-control" type="text" name="code" id="code" required="" placeholder="Enter your code">
+                                                @error('code')
                                                     <small class="text-danger">{{$message}}</small>
                                                 @enderror
-                                            </div>
-
-                                            <div class="form-group mb-3">
-                                                <label for="password" class="form-label">Password</label>
-                                                <input class="form-control" type="password" name="password" required="" id="password" placeholder="Enter your password">
-                                                 @error('password ')
-                                                    <small class="text-danger">{{$message}}</small>
-                                                @enderror
-                                            </div>
-
-                                            <div class="form-group d-flex mb-3">
-                                                <div class="col-sm-6">
-                                                    <div class="form-check">
-                                                        <input type="checkbox" class="form-check-input" id="checkbox-signin" name="remember" checked>
-                                                        <label class="form-check-label" for="checkbox-signin">Remember me</label>
-                                                    </div>
-                                                </div>
-                                                <div class="col-sm-6 text-end">
-                                                    <a class="text-muted fs-14" href="{{ route('password.request') }}">Forgot password?</a>
-                                                </div>
                                             </div>
 
                                             <div class="form-group mb-0 row">
                                                 <div class="col-12">
                                                     <div class="d-grid">
-                                                        <button class="btn btn-primary" type="submit"> Log In </button>
+                                                        <button class="btn btn-primary" type="submit"> Verify </button>
                                                     </div>
                                                 </div>
                                             </div>
